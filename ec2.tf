3d87011627d9c7000887cc5f10394da8e1886b78
kicinskido <kicinskid@gmail.com>
2023-08-18 18:24:16 +0000

    jakies formatowanie

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "latest_ubuntu" {
-  owners           = ["099720109477"]
+  owners      = ["099720109477"]
   most_recent = true
 
   filter {
@@ -9,14 +9,14 @@ data "aws_ami" "latest_ubuntu" {
 }
 
 resource "aws_instance" "panda" {
-  depends_on = [null_resource.download_ssh_key]
+  depends_on             = [null_resource.download_ssh_key]
   count                  = length(var.availability_zones)
   ami                    = data.aws_ami.latest_ubuntu.image_id
   instance_type          = "t2.micro"
   availability_zone      = var.availability_zones[count.index]
   key_name               = var.aws_key_name
   vpc_security_group_ids = [aws_security_group.sg_pub.id]
-  subnet_id = aws_default_subnet.default_az[count.index].id
+  subnet_id              = aws_default_subnet.default_az[count.index].id
 }
 
 resource "aws_security_group" "sg_pub" {
